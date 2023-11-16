835f2d58d624da77d32cefe90acee2290abd0c32
Shad Mirza <shadmirza100@gmail.com>
2023-11-17 00:43:32 +0530

    hardcoded path param removed , added dynamic path param

--- src/app/[url]/route.ts
+++ src/app/[url]/route.ts
@@ -3,14 +3,17 @@ import {connectDB} from '@/app/utils/database'
 import {Link} from '@/app/models/link'
 
 export async function GET(req:NextRequest, res:NextResponse) {
+  const linkParam = req.nextUrl.pathname.slice(1)
+  console.log(req.nextUrl.origin);
+  
   await connectDB()
-  let url ='';
   try{
-    const link = await Link.findOne({generatedLink: "shadmirza"}).select("longLink")
-    url = link?.longLink || ''
+    const link = await Link.findOne({generatedLink: linkParam}).select("longLink")
+    let url = link?.longLink || req.nextUrl.origin
+    return NextResponse.redirect(url)
   }
   catch(err){
     console.log('error', err);
+    return NextResponse.json({},{status:500})
   }
-  return NextResponse.redirect(url)
 }
